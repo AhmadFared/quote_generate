aafcf7e9d0411c0b6d15a1c21e2e661f4cf160d3
Ahmad Fared <ahmadfaredd8@gmail.com>
2021-07-09 22:32:17 +0300

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import image from "./body.jpg";
 
 function App() {
+  const [advice, setAdvice] = useState([]);
+  const axios = require("axios").default;
+
+  const generateAdvice = () =>
+    axios
+      .get("https://api.adviceslip.com/advice")
+      .then((response) => {
+        setAdvice(response.data.slip.advice);
+        console.log(response.data.slip);
+      })
+      .catch((error) => console.log(error));
+
+  useEffect(() => {
+    generateAdvice();
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main-body container-fluid">
+      <div className="advice-container">
+        <div className="advice">{advice}</div>
+        {/* <div className="advice">Hello go bring your pet</div> */}
+        <div className="generate">
+          <button className="button btn-4" onClick={generateAdvice}>
+            Give me an Advice!
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
